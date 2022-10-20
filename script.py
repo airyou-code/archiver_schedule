52bc5ab11ccb2ba94c9efeeb1e0d3129d349b6ed
airyou-code <holod7272@outlook.com>
2022-10-21 00:13:44 +0200

    first commit

--- script.py
+++ script.py
@@ -2,35 +2,46 @@
 #-*- coding: utf-8 -*-
 import os
 import argparse
+import gzip
 # sudo echo -e '*/1 * * * * /root/task/script.py' | sudo crontab -
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 parser = argparse.ArgumentParser(description='A tutorial of argparse!')
 parser.add_argument("-a", action="store_true", help="This is the 'a' variable")
-parser.add_argument("-d", default="/", help="This is the 'b' variable")
+parser.add_argument("-d", default="/var/log", help="This is the 'b' variable")
 args = parser.parse_args()
-a = args.a
-b = args.d
+activate = args.a
+dir = args.d
 
-def sendmessage(dir="/var/log/"):
+def archiver(dir="/var/log/"):
+    dir_name = dir.split("/")[-1]
+    os.chdir("..")
+    os.system(f"gzip -r {dir_name}")
+    os.chdir(dir_name)
+
     # print(os.path.join(f"{BASE_DIR}{dir}"))
+    # os.system("gzip -cf log.gz icon.txt icon2.txt")
+    # os.system("gunzip icon.txt.gz icon2.txt.gz")
         
-    my_file = open("BabyFile.txt", "w+")
-    my_file.write(f"Привет, файл! {BASE_DIR}")
-    my_file.close()
+    # my_file = open("BabyFile.txt", "w+")
+    # my_file.write(f"Привет, файл! {BASE_DIR}")
+    # my_file.close()
 
 def main():
     os.chdir("/")    
     try:
-        os.chdir(os.path.join(f"{BASE_DIR + args.d}"))
+        os.chdir(os.path.join(f"{BASE_DIR + dir}"))
     except:
         print(f"ERROR: Couldn't find the {BASE_DIR + args.d} directory")
         return 1
 
-    if a:
-        sendmessage(args.d)
+
+    if activate:
+        archiver(args.d)
     else:
-        os.system(f"sudo echo -e '*/1 * * * * {BASE_DIR}/script.py -a' | sudo crontab -")
+        os.system(f"sudo echo -e '*/1 * * * * {BASE_DIR}/script.py -a -d {dir} ' | sudo crontab -")
         os.system("sudo service cron start")
+        
+
 
 if __name__ == '__main__':
     main()
