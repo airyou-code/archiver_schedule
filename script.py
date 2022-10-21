87a66c2d8eec4134eaf4e22da377d61582faa7e1
Artem Kirillov <74914141+airyou-code@users.noreply.github.com>
2022-10-21 12:48:46 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 import os
@@ -5,11 +6,18 @@ import argparse
 import gzip
 # sudo echo -e '*/1 * * * * /root/task/script.py' | sudo crontab -
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-parser = argparse.ArgumentParser(description='A tutorial of argparse!')
-parser.add_argument("-a", action="store_true", help="This is the 'a' variable")
-parser.add_argument("-d", default="/var/log", help="This is the 'b' variable")
+parser = argparse.ArgumentParser(description='archives in a folder every t=30 days,\nstandard directory: /var/log')
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-a", action="store_true", help="compress now")
+group.add_argument("-u", action="store_true", help="uncompress now")
+parser.add_argument("-s", action="store_true", help="stop cron")
+parser.add_argument("-d", default="/var/log", help="Сhange the directory")
+parser.add_argument("-t", default=30, type=int, help="Change the time after which the script will be activated")
+
 args = parser.parse_args()
 activate = args.a
+s = args.s
+u = args.u
 dir = args.d
 
 def archiver(dir="/var/log/"):
@@ -18,13 +26,11 @@ def archiver(dir="/var/log/"):
     os.system(f"gzip -r {dir_name}")
     os.chdir(dir_name)
 
-    # print(os.path.join(f"{BASE_DIR}{dir}"))
-    # os.system("gzip -cf log.gz icon.txt icon2.txt")
-    # os.system("gunzip icon.txt.gz icon2.txt.gz")
-        
-    # my_file = open("BabyFile.txt", "w+")
-    # my_file.write(f"Привет, файл! {BASE_DIR}")
-    # my_file.close()
+def uncompress(dir="/var/log/"):
+    dir_name = dir.split("/")[-1]
+    os.chdir("..")
+    os.system(f"gunzip -r {dir_name}")
+    os.chdir(dir_name)
 
 def main():
     os.chdir("/")    
@@ -34,11 +40,20 @@ def main():
         print(f"ERROR: Couldn't find the {BASE_DIR + args.d} directory")
         return 1
 
+    if s:
+        ans = input("This action will DELETE ALL the cron tasks of the root user\nView tasks, command: sudo crontab -u root -e")
+        if ans == 'y':
+            os.system("sudo crontab -u root -r")
+        return 0
+
+    if u:
+        uncompress(args.d)
+        return 0
 
     if activate:
         archiver(args.d)
     else:
-        os.system(f"sudo echo -e '*/1 * * * * {BASE_DIR}/script.py -a -d {dir} ' | sudo crontab -")
+        os.system(f"sudo echo -e '*/{args.t} * * * * {BASE_DIR}/script.py -a -d {dir} ' | sudo crontab -")
         os.system("sudo service cron start")
         
 
